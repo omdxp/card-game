ba19c499b041409ee19979434e894169d912a5e1
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-02-13 10:03:58 +0100

    adding background and change some fonts

--- Card Game/ContentView.swift
+++ Card Game/ContentView.swift
@@ -9,30 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Image("logo")
-            Spacer()
-            HStack {
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
                 Spacer()
-                Image("card3")
+                Image("logo")
                 Spacer()
-                Image("card4")
-                Spacer()
-            }
-            Spacer()
-            Image("dealbutton")
-            Spacer()
-            HStack {
-                Spacer()
-                VStack {
-                    Text("Player")
-                    Text("0")
+                HStack {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
                 }
                 Spacer()
-                VStack {
-                    Text("CPU")
-                    Text("0")
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
                 }
                 Spacer()
             }
