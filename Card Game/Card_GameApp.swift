0a6ae581b840fb566e51ddaf7d21a8dbcc1fbe4b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-02-13 09:31:49 +0100

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Omar Belghaouti on 13/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
